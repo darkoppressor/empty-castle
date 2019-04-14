c6ec25286adefa7cdb5ea5f8b9a7ede4c5fdae6a
Kevin Wells <darkoppressor@gmail.com>
2019-04-14 18:37:03 -0500

    Fix compiler errors

--- dependencies/libtcod/bresenham.h
+++ dependencies/libtcod/bresenham.h
@@ -32,54 +32,25 @@
 #ifndef _TCOD_BRESENHAM_H
 #define _TCOD_BRESENHAM_H
 
-#include "portability.h"
+#include <stdbool.h>
 
-#ifdef __cplusplus
 extern "C" {
-#endif
-/**
- *  \brief A callback to be passed to TCOD_line
- *
- *  The points given to the callback include both the starting and ending
- *  positions.
- *
- *  \param x
- *  \param y
- *  \return As long as this callback returns true it will be called with the
- *          next x,y point on the line.
- */
-typedef bool (*TCOD_line_listener_t) (int x, int y);
+	/**
+	 *  \brief A struct used for computing a bresenham line.
+	 */
+	typedef struct {
+		int stepx;
+		int stepy;
+		int e;
+		int deltax;
+		int deltay;
+		int origx;
+		int origy;
+		int destx;
+		int desty;
+	} TCOD_bresenham_data_t;
 
-TCODLIB_API TCOD_DEPRECATED(
-    "This function is not reentrant. Use TCOD_line_init_mt instead.")
-void TCOD_line_init(int xFrom, int yFrom, int xTo, int yTo);
-
-TCODLIB_API TCOD_DEPRECATED("This function is not reentrant.")
-bool TCOD_line_step(int *xCur, int *yCur); /* advance one step. returns true if we reach destination */
-
-/* atomic callback function. Stops when the callback returns false */
-TCODLIB_API bool TCOD_line(int xFrom, int yFrom, int xTo, int yTo, TCOD_line_listener_t listener);
-/**
- *  \brief A struct used for computing a bresenham line.
- */
-typedef struct {
-	int stepx;
-	int stepy;
-	int e;
-	int deltax;
-	int deltay;
-	int origx;
-	int origy;
-	int destx;
-	int desty;
-} TCOD_bresenham_data_t;
-
-TCODLIB_API void TCOD_line_init_mt(int xFrom, int yFrom, int xTo, int yTo, TCOD_bresenham_data_t *data);
-TCODLIB_API bool TCOD_line_step_mt(int *xCur, int *yCur, TCOD_bresenham_data_t *data);
-
-TCOD_DEPRECATED("Use TCOD_line instead.")
-TCODLIB_API bool TCOD_line_mt(int xFrom, int yFrom, int xTo, int yTo, TCOD_line_listener_t listener, TCOD_bresenham_data_t *data);
-#ifdef __cplusplus
+	void TCOD_line_init_mt(int xFrom, int yFrom, int xTo, int yTo, TCOD_bresenham_data_t *data);
+	bool TCOD_line_step_mt(int *xCur, int *yCur, TCOD_bresenham_data_t *data);
 }
-#endif
 #endif
