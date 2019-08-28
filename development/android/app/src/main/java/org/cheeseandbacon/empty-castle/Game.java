592bb68bee4bb2f7c391c4207c34bdfd5736bb55
Kevin Wells <darkoppressor@gmail.com>
2019-08-27 21:57:47 -0400

    Update SDL2 libs for Android

---
+++ development/android/app/src/main/java/org/cheeseandbacon/empty-castle/Game.java
@@ -0,0 +1,6 @@
+package org.cheeseandbacon.emptycastle;
+
+import org.libsdl.app.SDLActivity;
+
+public class Game extends SDLActivity {
+}
--- development/android/src/org/cheeseandbacon/empty-castle/Game.java
+++
@@ -1,5 +0,0 @@
-package org.cheeseandbacon.emptycastle;
-
-import org.libsdl.app.SDLActivity;
-
-public class Game extends SDLActivity { }
