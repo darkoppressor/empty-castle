628485663e6c798c29b6562ca2ea8761114e0ded
Kevin Wells <darkoppressor@gmail.com>
2025-03-03 19:16:23 -0500

    Update copyright
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- field_of_view_source.h
+++ field_of_view_source.h
@@ -1,6 +1,4 @@
-/* Copyright (c) 2012 Cheese and Bacon Games, LLC */
-/* This file is licensed under the MIT License. */
-/* See the file docs/LICENSE.txt for the full license text. */
+/* Copyright (c) Cheese and Bacon Games, LLC. See docs/LICENSE.md for details. */
 /* This file incorporates code from libtcod. See the file docs/third-party-licenses/libtcod.txt. */
 
 #ifndef field_of_view_source_h
